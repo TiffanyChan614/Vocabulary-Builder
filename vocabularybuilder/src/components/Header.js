4cd7097d1f700e939b7259325111a96d91a271f5
TiffChan <tiffanychan1999614@gmail.com>
2023-08-12 19:13:39 -0400

    Style header

--- vocabularybuilder/src/components/Header.js
+++ vocabularybuilder/src/components/Header.js
@@ -1,21 +1,73 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import { AiOutlineHome, AiOutlineSearch } from 'react-icons/ai'
+import { BiBookBookmark } from 'react-icons/bi'
+import { useState, useEffect } from 'react'
 
 const Header = () => {
+  const activeTextClassName = 'font-bold text-gray-800 underline'
+  const textLinkClassName = 'hover:underline'
+  const iconLinkClassName =
+    'hover:border-b-2 hover:pb-1 border-gray-700 text-gray-700'
+  const activeIconClassName = 'border-b-2 border-gray-800 text-gray-800 pb-1'
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 500)
+    }
+
+    handleResize()
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  const navigationItems = [
+    {
+      text: 'Home',
+      icon: <AiOutlineHome size={25} />,
+      link: '/',
+    },
+    {
+      text: 'Search',
+      icon: <AiOutlineSearch size={25} />,
+      link: '/search',
+    },
+    {
+      text: 'Your Journal',
+      icon: <BiBookBookmark size={25} />,
+      link: '/journal',
+    },
+  ]
+
   return (
-    <header>
-      <Link
-        className='site-logo'
-        to='/'>
-        Home
-      </Link>
-      <nav>
-        <NavLink to='/search'>Search</NavLink>
-      </nav>
-      <nav>
-        <NavLink to='/journal'>Journal</NavLink>
-      </nav>
-    </header>
+    <div className='header-wrapper px-3 py-2'>
+      <header>
+        <nav className='flex flex-row items-center px-3 py-3 border-b-2 gap-4 font-medium text-gray-700 sm:gap-6'>
+          {navigationItems.map((item) => (
+            <NavLink
+              key={item.link}
+              className={({ isActive }) =>
+                isActive ? activeTextClassName : textLinkClassName
+              }
+              to={item.link}>
+              {({ isActive }) =>
+                isMobile ? (
+                  <div
+                    className={
+                      isActive ? activeIconClassName : iconLinkClassName
+                    }>
+                    {item.icon}
+                  </div>
+                ) : (
+                  item.text
+                )
+              }
+            </NavLink>
+          ))}
+        </nav>
+      </header>
+    </div>
   )
 }
 
--- vocabularybuilder/src/components/Layout.js
+++ vocabularybuilder/src/components/Layout.js
@@ -5,7 +5,7 @@ import Header from './Header'
 const Layout = () => {
   return (
     <div className='site-wrapper'>
-      <Header />
+      <Header className='flex flex-row items-center px-2 py-2 bg-gray-400' />
       <main>
         <Outlet />
       </main>
