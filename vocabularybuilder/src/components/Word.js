cf68a0b38cebdc96527fa4f0f7c94491a7ca92c9
TiffChan <tiffanychan1999614@gmail.com>
2023-08-13 19:23:38 -0400

    Abstract WordHeader from Word

--- vocabularybuilder/src/components/Word.js
+++ vocabularybuilder/src/components/Word.js
@@ -1,8 +1,7 @@
 import { useState } from 'react'
 import WordForm from './WordForm'
 import WordDetails from './WordDetails'
-import { AiFillSound, AiOutlinePlus } from 'react-icons/ai'
-import { FiMoreHorizontal } from 'react-icons/fi'
+import WordHeader from './WordHeader'
 
 const Word = ({ wordData, page, handleDelete, setWords }) => {
   console.log('word', wordData)
@@ -40,61 +39,17 @@ const Word = ({ wordData, page, handleDelete, setWords }) => {
   if (wordData.word === 'no word') {
     return <div className='word'>No word found</div>
   } else {
-    const {
-      word,
-      pronunciation,
-      partOfSpeech,
-      definition,
-      synonyms,
-      antonyms,
-      examples,
-      images,
-    } = wordData
+    const { definition, synonyms, antonyms, examples, images } = wordData
     return (
       <>
         <div className='word flex flex-col p-3 rounded-xl border-2 border-indigo-100 gap-3'>
-          <div className='word--header flex justify-between'>
-            <div className='flex gap-5 items-center'>
-              <div className='flex gap-1 items-center'>
-                <h3 className='text-xl font-bold text-indigo-800'>{word}</h3>
-                {pronunciation && (
-                  <h4 className='text-lg'>{`[${pronunciation}]`}</h4>
-                )}
-                <button
-                  className='word--audio'
-                  onClick={(e) => speak(e, word, 'samantha')}>
-                  <AiFillSound size={20} />
-                </button>
-              </div>
-
-              {partOfSpeech && (
-                <h4 className='font-semibold text-md'>
-                  {partOfSpeech[0].toUpperCase() + partOfSpeech.slice(1)}
-                </h4>
-              )}
-            </div>
-
-            <div className='flex items-center gap-3'>
-              {(synonyms?.length > 0 ||
-                antonyms?.length > 0 ||
-                examples?.length > 0 ||
-                images?.length > 0) && (
-                <button onClick={() => setShowDetails((prevShow) => !prevShow)}>
-                  <FiMoreHorizontal size={20} />
-                </button>
-              )}
-
-              {page === 'search' && (
-                <button
-                  onClick={(e) => {
-                    e.stopPropagation()
-                    setShowForm(true)
-                  }}>
-                  <AiOutlinePlus size={20} />
-                </button>
-              )}
-            </div>
-          </div>
+          <WordHeader
+            wordData={wordData}
+            page={page}
+            speak={speak}
+            setShowDetails={setShowDetails}
+            setShowForm={setShowForm}
+          />
           <div className='word--definition'>
             {transformSentence(definition) || 'No definition found'}
           </div>
---
+++ vocabularybuilder/src/components/WordHeader.js
@@ -0,0 +1,57 @@
+import { AiFillSound, AiOutlinePlus } from 'react-icons/ai'
+import { FiMoreHorizontal } from 'react-icons/fi'
+const WordHeader = ({ wordData, page, speak, setShowDetails, setShowForm }) => {
+  const {
+    word,
+    pronunciation,
+    partOfSpeech,
+    synonyms,
+    antonyms,
+    examples,
+    images,
+  } = wordData
+  return (
+    <div className='word--header flex justify-between'>
+      <div className='flex gap-5 items-center'>
+        <div className='flex gap-1 items-center'>
+          <h3 className='text-xl  font-bold text-indigo-800'>{word}</h3>
+          <h4 className='text-lg'>{`[${pronunciation}]`}</h4>
+          <button
+            className='word--audio'
+            onClick={(e) => speak(e, word, 'samantha')}>
+            <AiFillSound size={20} />
+          </button>
+        </div>
+
+        {partOfSpeech && (
+          <h4 className='font-semibold text-md'>
+            {partOfSpeech[0].toUpperCase() + partOfSpeech.slice(1)}
+          </h4>
+        )}
+      </div>
+
+      <div className='flex items-center gap-3'>
+        {(synonyms?.length > 0 ||
+          antonyms?.length > 0 ||
+          examples?.length > 0 ||
+          images?.length > 0) && (
+          <button onClick={() => setShowDetails((prevShow) => !prevShow)}>
+            <FiMoreHorizontal size={20} />
+          </button>
+        )}
+
+        {page === 'search' && (
+          <button
+            onClick={(e) => {
+              e.stopPropagation()
+              setShowForm(true)
+            }}>
+            <AiOutlinePlus size={20} />
+          </button>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default WordHeader
