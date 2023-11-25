e70fb952fb1b58610c59c5b1be3ec1b3222e0644
TiffChan <tiffanychan1999614@gmail.com>
2023-11-25 09:42:20 -0500

    Add usersController

---
+++ backend/controllers/users.js
@@ -0,0 +1,25 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.post('/', async(request, response) => {
+    const { username, name, password } = request.body
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    const user = new User({
+        username, name, passwordHash
+    })
+
+    const savedUser = await user.save()
+
+    response.status(201).json(savedUser)
+})
+
+usersRouter.get('/', async(request, response) => {
+    const users = await User.find({}).populate('journal', { word: 1 })
+    response.json(users)
+})
+
+module.exports = usersRouter
